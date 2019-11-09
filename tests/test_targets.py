8ad3c68a108ed7d2e676313a384d2488ae307f73
Stair, Michael (ms1784) <ms1784@us.att.com>
2019-11-09 12:04:29 -0500

    updating unit tests

---
+++ tests/test_targets.py
@@ -0,0 +1,22 @@
+import lycan 
+import pytest
+
+import stix2.exceptions
+
+
+def test_ipv4_address_example():
+    ip4 = lycan.v10.IPv4Address(
+        ipv4_net="198.51.100.3")
+    
+    assert ip4.ipv4_net == "198.51.100.3"
+
+    ip4 = lycan.v10.IPv4Address(
+        ipv4_net="198.51.100.0/24")
+
+    assert ip4.ipv4_net == "198.51.100.0/24"
+
+    with pytest.raises(stix2.exceptions.MissingPropertiesError) as excinfo:
+        ip4 = lycan.v10.IPv4Address()
+    
+    assert excinfo.value.cls == lycan.v10.IPv4Address
+
