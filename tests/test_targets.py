57f5f97a8c51c167092495322d642096ee5d523c
Stair, Michael (ms1784) <ms1784@us.att.com>
2019-11-11 07:29:35 -0500

    rename import root

--- tests/test_targets.py
+++ tests/test_targets.py
@@ -5,18 +5,18 @@ import stix2.exceptions
 
 
 def test_ipv4_address_example():
-    ip4 = lycan.v10.IPv4Address(
+    ip4 = openc2.v10.IPv4Address(
         ipv4_net="198.51.100.3")
     
     assert ip4.ipv4_net == "198.51.100.3"
 
-    ip4 = lycan.v10.IPv4Address(
+    ip4 = openc2.v10.IPv4Address(
         ipv4_net="198.51.100.0/24")
 
     assert ip4.ipv4_net == "198.51.100.0/24"
 
     with pytest.raises(stix2.exceptions.MissingPropertiesError) as excinfo:
-        ip4 = lycan.v10.IPv4Address()
+        ip4 = openc2.v10.IPv4Address()
     
-    assert excinfo.value.cls == lycan.v10.IPv4Address
+    assert excinfo.value.cls == openc2.v10.IPv4Address
 
